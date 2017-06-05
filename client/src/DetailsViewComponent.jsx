a1058575b0a5631eaab4cca64bf100c59d294a64
Bartosz Oczujda <bartosz.oczujda@gmail.com>
2017-06-05 13:39:14 +0200

    Remove inline styles and add style.css

--- client/src/DetailsViewComponent.jsx
+++ client/src/DetailsViewComponent.jsx
@@ -11,33 +11,33 @@ class DetailsViewComponent extends React.Component {
 
 
 		return (
-			<div style={{display:"inline"}}>
+			<div>
 				<strong>Names:</strong>
-				<div style={{marginBottom:"10px"}}>
-					<span style={{marginRight:"10px"}}>{this.props.selectedMedicineDetails.english_name}</span>
-					<span style={{marginRight:"10px"}}>{this.props.selectedMedicineDetails.latin_name}</span>
-					<span style={{marginRight:"10px"}}>{this.props.selectedMedicineDetails.polish_name}</span>
-					<span style={{marginRight:"10px"}}>{this.props.selectedMedicineDetails.german_name}</span>
+				<div>
+					<span>{this.props.selectedMedicineDetails.english_name}</span>
+					<span>{this.props.selectedMedicineDetails.latin_name}</span>
+					<span>{this.props.selectedMedicineDetails.polish_name}</span>
+					<span>{this.props.selectedMedicineDetails.german_name}</span>
 				</div>
 				<strong>Ailments:</strong>
-				<div style={{marginBottom:"10px"}}>
-					<span style={{marginRight:"10px"}}>{this.props.selectedMedicineDetails.ailments}</span>
+				<div>
+					<span>{this.props.selectedMedicineDetails.ailments}</span>
 				</div>
 				<strong>Active ingredients:</strong>
-				<div style={{marginBottom:"10px"}}>
-					<span style={{marginRight:"10px"}}>{this.props.selectedMedicineDetails.active_ingredients}</span>
+				<div>
+					<span>{this.props.selectedMedicineDetails.active_ingredients}</span>
 				</div>
 				<strong>Organisms:</strong>
-				<div style={{marginBottom:"10px"}}>
-					<span style={{marginRight:"10px"}}>{this.props.selectedMedicineDetails.organisms}</span>
+				<div>
+					<span>{this.props.selectedMedicineDetails.organisms}</span>
 				</div>
 				<strong>Description:</strong>
-				<div style={{marginBottom:"10px"}}>
-                	<span style={{marginRight:"10px"}}>{this.props.selectedMedicineDetails.description}</span>
+				<div>
+                	<span>{this.props.selectedMedicineDetails.description}</span>
 				</div>
 				<strong>Additional info:</strong>
-				<div style={{marginBottom:"10px"}}>
-					<span style={{marginRight:"10px"}}>{this.props.selectedMedicineDetails.additional_info}</span>
+				<div>
+					<span>{this.props.selectedMedicineDetails.additional_info}</span>
 				</div>
 			</div>
 		);
